dc488812a4a463355ba9107c90b17a9d7c2cd8a4
tosdy <tosdy14th@gmail.com>
2022-01-06 16:09:57 +0700

    EP64

--- lib/states/edit_profile_saler.dart
+++ lib/states/edit_profile_saler.dart
@@ -1,12 +1,17 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_collection_literals
 
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shoppingmall/models/user_model.dart';
 import 'package:shoppingmall/utility/my_constant.dart';
+import 'package:shoppingmall/widgets/show_image.dart';
+import 'package:shoppingmall/widgets/show_process.dart';
 import 'package:shoppingmall/widgets/show_title.dart';
 
 class EditProfileSaler extends StatefulWidget {
@@ -21,10 +26,32 @@ class _EditProfileSalerState extends State<EditProfileSaler> {
   TextEditingController nameController = TextEditingController();
   TextEditingController addressController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
+  LatLng? latLng;
   @override
   void initState() {
     super.initState();
     findUser();
+    findLatLng();
+  }
+
+  Future<Null> findLatLng() async {
+    Position? position = await findPosition();
+
+    if (position != null) {
+      setState(() {
+        latLng = LatLng(position.latitude, position.longitude);
+      });
+    }
+  }
+
+  Future<Position?> findPosition() async {
+    Position? position;
+    try {
+      position = await Geolocator.getCurrentPosition();
+    } catch (e) {
+      position = null;
+    }
+    return position;
   }
 
   Future<Null> findUser() async {
@@ -61,9 +88,92 @@ class _EditProfileSalerState extends State<EditProfileSaler> {
             buildName(constraints),
             buildAddress(constraints),
             buildPhone(constraints),
+            buildTitle('Avatar : '),
+            buildAvatar(constraints),
+            buildTitle('Location :'),
+            buildMap(constraints)
           ],
         ),
       ),
+    );
+  }
+
+  Row buildMap(BoxConstraints constraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          margin: EdgeInsets.symmetric(
+            vertical: 16,
+          ),
+          width: constraints.maxWidth * 0.75,
+          height: constraints.maxWidth * 0.5,
+          child: latLng == null
+              ? ShowProgress()
+              : GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: latLng!,
+                    zoom: 16,
+                  ),
+                  onMapCreated: (controller) {},
+                  markers: <Marker>[
+                    Marker(
+                      markerId: MarkerId('id'),
+                      position: latLng!,
+                      infoWindow: InfoWindow(
+                          title: 'Your Location',
+                          snippet:
+                              'lat : ${latLng!.latitude} Lng : ${latLng!.longitude}'),
+                    )
+                  ].toSet(),
+                ),
+        ),
+      ],
+    );
+  }
+
+  Row buildAvatar(BoxConstraints constraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 16),
+          decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add_a_photo),
+                color: MyConstant.dark,
+              ),
+              Container(
+                height: constraints.maxWidth * 0.6,
+                width: constraints.maxWidth * 0.6,
+                child: userModel == null
+                    ? ShowProgress()
+                    : Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: userModel!.avatar == null
+                            ? Showimage(path: MyConstant.avatar)
+                            : CachedNetworkImage(
+                                imageUrl:
+                                    '${MyConstant.domain}${userModel!.avatar}',
+                                placeholder: (context, url) => ShowProgress(),
+                              ),
+                      ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add_photo_alternate),
+                color: MyConstant.dark,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
@@ -111,7 +221,7 @@ class _EditProfileSalerState extends State<EditProfileSaler> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          margin: EdgeInsets.only(top: 16),
+          margin: EdgeInsets.symmetric(vertical: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
             controller: phoneController,
