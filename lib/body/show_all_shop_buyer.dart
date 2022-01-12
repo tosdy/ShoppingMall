b41e22c15337d7199642a364de19728257765265
tosdy <tosdy14th@gmail.com>
2022-01-12 09:07:25 +0700

    EP70_1

---
+++ lib/body/show_all_shop_buyer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShowAllShopBuyer extends StatefulWidget {
+  const ShowAllShopBuyer({Key? key}) : super(key: key);
+
+  @override
+  _ShowAllShopBuyerState createState() => _ShowAllShopBuyerState();
+}
+
+class _ShowAllShopBuyerState extends State<ShowAllShopBuyer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('ShowAllShopBuyer'),
+    );
+  }
+}
--- lib/states/buyer_service.dart
+++ lib/states/buyer_service.dart
@@ -2,6 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shoppingmall/body/my_money_buyer.dart';
+import 'package:shoppingmall/body/my_order_buyer.dart';
+import 'package:shoppingmall/body/show_all_shop_buyer.dart';
 import 'package:shoppingmall/utility/my_constant.dart';
 import 'package:shoppingmall/widgets/show_signout.dart';
 import 'package:shoppingmall/widgets/show_title.dart';
@@ -14,6 +17,9 @@ class BuyerService extends StatefulWidget {
 }
 
 class _BuyerServiceState extends State<BuyerService> {
+  List<Widget> Widgets = [ShowAllShopBuyer(), MyMoneyBuyer(), MyOrderBuyer()];
+  int indexWidget = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +27,84 @@ class _BuyerServiceState extends State<BuyerService> {
         title: Text('Buyer'),
       ),
       drawer: Drawer(
-        child: ShowSignOut(),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                buildHeader(),
+                buildShowAllShop(),
+                buildMyMoney(),
+                buildShowMyOrder()
+              ],
+            ),
+            ShowSignOut(),
+          ],
+        ),
       ),
+      body: Widgets[indexWidget],
     );
   }
+
+  ListTile buildShowAllShop() {
+    return ListTile(
+      leading: Icon(
+        Icons.shopping_bag,
+        size: 36,
+        color: MyConstant.dark,
+      ),
+      title: ShowTitle(
+        title: 'Show Shop',
+        textStyle: MyConstant().h2Style(),
+      ),
+      subtitle: ShowTitle(
+          title: 'แสดงร้านค้า ทั้งหมด', textStyle: MyConstant().h3Style()),
+      onTap: () => setState(() {
+        indexWidget = 0;
+        Navigator.pop(context);
+      }),
+    );
+  }
+
+  ListTile buildMyMoney() {
+    return ListTile(
+      leading: Icon(
+        Icons.money,
+        size: 36,
+        color: MyConstant.dark,
+      ),
+      title: ShowTitle(
+        title: 'My Money',
+        textStyle: MyConstant().h2Style(),
+      ),
+      subtitle: ShowTitle(
+          title: 'แสดงจำนวนเงินที่มี', textStyle: MyConstant().h3Style()),
+      onTap: () => setState(() {
+        indexWidget = 1;
+        Navigator.pop(context);
+      }),
+    );
+  }
+
+  ListTile buildShowMyOrder() {
+    return ListTile(
+      leading: Icon(
+        Icons.list,
+        size: 36,
+        color: MyConstant.dark,
+      ),
+      title: ShowTitle(
+        title: 'My order',
+        textStyle: MyConstant().h2Style(),
+      ),
+      subtitle:
+          ShowTitle(title: 'แสดงการสั่งของ', textStyle: MyConstant().h3Style()),
+      onTap: () => setState(() {
+        indexWidget = 2;
+        Navigator.pop(context);
+      }),
+    );
+  }
+
+  UserAccountsDrawerHeader buildHeader() =>
+      UserAccountsDrawerHeader(accountName: null, accountEmail: null);
 }
