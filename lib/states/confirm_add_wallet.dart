b7f1236c5530714653d875ddedbd88c4247c3bc1
tosdy <tosdy14th@gmail.com>
2022-01-28 10:36:18 +0700

    EP109_1

--- lib/states/confirm_add_wallet.dart
+++ lib/states/confirm_add_wallet.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: prefer_const_constructors, prefer_if_null_operators
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:shoppingmall/utility/my_constant.dart';
+import 'package:shoppingmall/widgets/show_image.dart';
+import 'package:shoppingmall/widgets/show_title.dart';
 
 class ComfirmAddWallet extends StatefulWidget {
   const ComfirmAddWallet({Key? key}) : super(key: key);
@@ -11,6 +16,24 @@ class ComfirmAddWallet extends StatefulWidget {
 }
 
 class _ComfirmAddWalletState extends State<ComfirmAddWallet> {
+  String? dateTimeStr;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    findCurrnttime();
+  }
+
+  void findCurrnttime() {
+    DateTime dateTime = DateTime.now();
+    DateFormat dateFormat = DateFormat('dd/MM/yyyy HH:mm');
+
+    setState(() {
+      dateTimeStr = dateFormat.format(dateTime);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +46,55 @@ class _ComfirmAddWalletState extends State<ComfirmAddWallet> {
                 ? Icon(Icons.arrow_back_ios)
                 : Icon(Icons.arrow_back)),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          newHeadTitle(),
+          newDateTime(),
+          Spacer(),
+          newImage(),
+          Spacer(),
+          newButton(),
+        ],
+      ),
     );
+  }
+
+  Container newButton() {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('Confirm Add Wallet'),
+      ),
+    );
+  }
+
+  Row newImage() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.add_a_photo)),
+        Container(
+          width: 200,
+          height: 200,
+          child: Showimage(path: MyConstant.image6),
+        ),
+        IconButton(onPressed: () {}, icon: Icon(Icons.add_photo_alternate)),
+      ],
+    );
+  }
+
+  ShowTitle newDateTime() {
+    return ShowTitle(
+      title: dateTimeStr == null ? 'dd/MM/yyyy HH:mm' : dateTimeStr!,
+      textStyle: MyConstant().h2BlueStyle(),
+    );
+  }
+
+  ShowTitle newHeadTitle() {
+    return ShowTitle(
+        title: 'Current Date Payment', textStyle: MyConstant().h1Style());
   }
 }
